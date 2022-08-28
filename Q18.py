f42a6e8ec81c1e8ce9e00a83b418902031f0ae7b
Heewon318 <110396828+Heewon318@users.noreply.github.com>
2022-08-28 23:04:42 +0900

    Create Q18.py

---
+++ Q18.py
@@ -0,0 +1,26 @@
+# 홀짝 연결 리스트 (p.233)
+
+from typing import * 
+ 
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = None
+        
+def oddEvenList(head : ListNode) -> ListNode:
+    # 예외 처리
+    if head is None:
+        return None
+
+    odd = head
+    even = head.next
+    even_head = head.next
+
+    # 반복하면서 홀짝 노드 처리
+    while even and even.next:
+        odd.next, even.next = odd.next.next, even.next.next
+        odd, even = odd.next, even.next
+
+    # 홀수 노드의 마지막을 짝수 헤드로 연결
+    odd.next = even_head
+    return head
